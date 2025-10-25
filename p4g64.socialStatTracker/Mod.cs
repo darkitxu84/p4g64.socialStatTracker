eb6758e54a6413442775babac817a0fdbe28b538
darkitxu84 <76633266+darkitxu84@users.noreply.github.com>
2025-10-24 21:30:40 -0300

    Upload project

---
+++ p4g64.socialStatTracker/Mod.cs
@@ -0,0 +1,152 @@
+﻿using p4g64.socialStatTracker.Template;
+using p4g64.socialStatTracker.Configuration;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.Definitions.Enums;
+using Reloaded.Hooks.Definitions.X64;
+using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using System.Diagnostics;
+using static Reloaded.Hooks.Definitions.X64.FunctionAttribute;
+using IReloadedHooks = Reloaded.Hooks.ReloadedII.Interfaces.IReloadedHooks;
+using p3ppc.socialStatTracker;
+
+// Code adapted from Social Stat Tracker by AnimatedSwine
+// https://github.com/AnimatedSwine37/p3ppc.socialStatTracker
+
+namespace p4g64.socialStatTracker
+{
+    /// <summary>
+    /// Your mod logic goes here.
+    /// </summary>
+    public unsafe class Mod : ModBase // <= Do not Remove.
+    {
+        /// <summary>
+        /// Provides access to the mod loader API.
+        /// </summary>
+        private readonly IModLoader _modLoader;
+
+        /// <summary>
+        /// Provides access to the Reloaded.Hooks API.
+        /// </summary>
+        /// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
+        private readonly IReloadedHooks? _hooks;
+
+        /// <summary>
+        /// Provides access to the Reloaded logger.
+        /// </summary>
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Entry point into the mod, instance that created this class.
+        /// </summary>
+        private readonly IMod _owner;
+
+        /// <summary>
+        /// Provides access to this mod's configuration.
+        /// </summary>
+        private Config _configuration;
+
+        /// <summary>
+        /// The configuration of the currently executing mod.
+        /// </summary>
+        private readonly IModConfig _modConfig;
+
+
+        private IAsmHook _socialStatusLevelNameHook;
+        private IReverseWrapper<AppendPointsDelegate> _appendPointsReverseWrapper;
+        private short* _socialStatPoints;
+
+        const string getSocialStatLevelNameFuncSig = "48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 0F B7 F9 0F B7 DA 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 35 ?? ?? ?? ?? 8D 43 ?? 48 83 C6 08";
+        const int socialStatPointsOffset = 0x51BCD70 + 0xCA8;
+
+        public Mod(ModContext context)
+        {
+            _modLoader = context.ModLoader;
+            _hooks = context.Hooks;
+            _logger = context.Logger;
+            _owner = context.Owner;
+            _configuration = context.Configuration;
+            _modConfig = context.ModConfig;
+            nint baseAddress = Process.GetCurrentProcess().MainModule!.BaseAddress;
+
+            Utils.Initialise(_logger, _configuration);
+            IntPtr targetAddress = IntPtr.Add(baseAddress, socialStatPointsOffset);
+
+            _socialStatPoints = (short*)targetAddress.ToPointer();
+
+            var startupScannerController = _modLoader.GetController<IStartupScanner>();
+            if (startupScannerController == null || !startupScannerController.TryGetTarget(out var startupScanner))
+            {
+                Utils.LogError($"Unable to get controller for Reloaded SigScan Library, aborting initialisation");
+                return;
+            }
+
+            startupScanner.AddMainModuleScan(getSocialStatLevelNameFuncSig, result =>
+            {
+                if (!result.Found)
+                {
+                    Utils.LogError($"Unable to find GetSocialStatLevelName, aborting initialisation");
+                    return;
+                }
+                Utils.LogDebug($"found GetSocialStatLevelName function at 0x{result.Offset + baseAddress:X}");
+
+                string[] function =
+                {
+                    "use64",
+                    "push rcx\npush rdx\npush r8\npush r9\npush r10\npush r11",
+                    "sub rsp, 32",
+                    $"{_hooks.Utilities.GetAbsoluteCallMnemonics(AppendPoints, out _appendPointsReverseWrapper)}",
+                    "add rsp, 32",
+                    "pop r11\npop r10\npop r9\npop r8\npop rdx\npop rcx"
+                };
+                // baseAdress + offset points to the function begin, so we add an extra offset
+                _socialStatusLevelNameHook = _hooks.CreateAsmHook(function, baseAddress + result.Offset + 176, AsmHookBehaviour.ExecuteAfter).Activate();
+            });
+        }
+
+        // stat is actually stat index
+        private string AppendPoints(string message, short stat, short statLevel)
+        {
+            // remove this logs for mod release
+            _logger.WriteLineAsync($"[INFO] (Social Stat Tracker) stat index: {stat}");
+            _logger.WriteLineAsync($"[INFO] (Social Stat Tracker) points: {_socialStatPoints[stat]}");
+            return $"{message} p:{_socialStatPoints[stat]}";
+
+            /*
+            short points = _socialStatPoints[stat];
+            short lastRequired = _pointsRequired[stat][statLevel - 1];
+
+            if (statLevel == 6)
+            {
+                if (!_configuration.ShowAboveMax || points - lastRequired == 0)
+                    return message;
+                return $"{message} +{points - lastRequired}";
+            }
+
+            short required = _pointsRequired[stat][statLevel];
+
+            return $"{message} {points - lastRequired}/{required - lastRequired}";
+            */
+        }
+
+        [Function(new Register[] { Register.rax, Register.rdi, Register.r8 }, Register.rax, true )]
+        private delegate string AppendPointsDelegate(string message, short stat, short statLevel);
+
+        #region Standard Overrides
+        public override void ConfigurationUpdated(Config configuration)
+        {
+            // Apply settings from configuration.
+            // ... your code here.
+            _configuration = configuration;
+            _logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+        }
+        #endregion
+
+        #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Mod() { }
+#pragma warning restore CS8618
+        #endregion
+    }
+}
---
+++ p4g64.socialStatTracker/Utils.cs
@@ -0,0 +1,46 @@
+﻿using p4g64.socialStatTracker.Configuration;
+using Reloaded.Mod.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3ppc.socialStatTracker
+{
+    internal class Utils
+    {
+        private static ILogger _logger;
+        private static Config _config;
+        internal static nint BaseAddress { get; private set; }
+
+        internal static void Initialise(ILogger logger, Config config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        internal static void LogDebug(string message)
+        {
+            // if (_config.DebugEnabled)
+                // _logger.WriteLine($"[Social Stat Tracker] {message}");
+        }
+
+        internal static void Log(string message)
+        {
+            _logger.WriteLine($"[Social Stat Tracker] {message}");
+        }
+
+        internal static void LogError(string message, Exception e)
+        {
+            _logger.WriteLine($"[Social Stat Tracker] {message}: {e.Message}", System.Drawing.Color.Red);
+        }
+
+        internal static void LogError(string message)
+        {
+            _logger.WriteLine($"[Social Stat Tracker] {message}", System.Drawing.Color.Red);
+        }
+
+    }
+}
