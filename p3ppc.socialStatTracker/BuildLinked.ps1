3dee1c5b85558487b752f6eacb23cfddee423300
darkitxu84 <76633266+darkitxu84@users.noreply.github.com>
2025-10-24 21:31:27 -0300

    remove old project

--- p3ppc.socialStatTracker/BuildLinked.ps1
+++
@@ -1,9 +0,0 @@
-# Set Working Directory
-Split-Path $MyInvocation.MyCommand.Path | Push-Location
-[Environment]::CurrentDirectory = $PWD
-
-Remove-Item "$env:RELOADEDIIMODS/p3ppc.socialStatTracker/*" -Force -Recurse
-dotnet publish "./p3ppc.socialStatTracker.csproj" -c Release -o "$env:RELOADEDIIMODS/p3ppc.socialStatTracker" /p:OutputPath="./bin/Release" /p:ReloadedILLink="true"
-
-# Restore Working Directory
-Pop-Location
